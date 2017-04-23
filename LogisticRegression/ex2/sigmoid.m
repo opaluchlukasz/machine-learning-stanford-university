43ec31a73249e859846f86bd392d949b4c4c156d
opaluch.lukasz <opaluch.lukasz@gmail.com>
2017-04-23 16:33:32 +0200

    ex2 - Logistic Regression

---
+++ LogisticRegression/ex2/sigmoid.m
@@ -0,0 +1,12 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+g =  1./(1 + repmat(e, [size(z, 1) size(z, 2)]) .^ (-1* z));
+
+% =============================================================
+
+end
---
+++ README.md
@@ -0,0 +1,2 @@
+# machine-learning-stanford-university
+Solutions for Stanford's Machine Learning course on Coursera
